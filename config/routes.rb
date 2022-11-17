e3d08fdd3ab5cace7fdc2280436e694db6fd92d2
Scheals <95084627+scheals@users.noreply.github.com>
2022-11-17 14:43:04 +0100

    feat: assign creators to events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  devise_for :users
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :events, only: [:index]
   # Defines the root path route ("/")
   root "events#index"
 end
---
+++ db/migrate/20221117122900_add_user_to_event.rb
@@ -0,0 +1,5 @@
+class AddUserToEvent < ActiveRecord::Migration[7.0]
+  def change
+    add_column :events, :creator_id, :integer, foreign_key: true
+  end
+end
