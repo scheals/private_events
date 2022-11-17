17832b0c3fb9e059eb3445cf76e98adb6bb7e030
Scheals <95084627+scheals@users.noreply.github.com>
2022-11-17 15:27:27 +0100

    feat: introduce creating events through a form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,27 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to events_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new, :create]
   # Defines the root path route ("/")
   root "events#index"
 end
