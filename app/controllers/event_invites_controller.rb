3560896f6f6b87f97f4399677bb7f70552835438
Scheals <95084627+scheals@users.noreply.github.com>
2022-11-30 23:21:02 +0100

    feat: add feedback on event registration

--- app/controllers/event_invites_controller.rb
+++ app/controllers/event_invites_controller.rb
@@ -1,11 +1,14 @@
 class EventInvitesController < ApplicationController
   def create
     @event_invite = EventInvite.new(invite_params)
+    @event = Event.find(@event_invite.event_id)
 
     if @event_invite.save
-      flash.now[:success] = "Successfully registered for #{Event.find(params[:id]).name}"
+      flash[:success] = "Successfully registered for #{@event.name}"
+      redirect_to event_path(@event), id: @event.id
     else
-      flash.now[:error] = 'Unable to register for the event.'
+      flash[:error] = "Unable to register for the event. Perhaps you've already registered?"
+      redirect_to event_path(@event), id: @event.id, status: :unprocessable_entity
     end
   end
 
