5098397586aacbbbf2aa40e8d58d020e54bba029
Scheals <95084627+scheals@users.noreply.github.com>
2022-12-02 02:29:21 +0100

    feat: add way to query past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_invites, foreign_key: :event_id
   has_many :attendees, through: :event_invites, class_name: 'User'
+
+  def self.past
+    where(date: (..Time.current))
+  end
 end
