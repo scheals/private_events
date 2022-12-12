fd292ae8749fcd8545e86290e76c1d2c81ddbbac
Scheals <95084627+scheals@users.noreply.github.com>
2022-12-12 07:04:29 +0100

    feat: add editing parties

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,6 +30,20 @@ class EventsController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def event_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :event_invites, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index, :new, :create, :show, :destroy]
+  resources :events, only: [:index, :new, :create, :show, :destroy, :edit, :update]
   # Defines the root path route ("/")
   root "events#index"
 end
